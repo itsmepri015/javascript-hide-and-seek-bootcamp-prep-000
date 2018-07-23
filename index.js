f2166b59d6c472c68ad220d81ff56eb731126889
naizal Edy <pritiadhikari@gmail.com>
2018-07-23 19:56:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector)
+{
+  return document.querySelector(selector)
+}
+
+function nestedTarget()
+{
+   return  document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n)
+{
+  
+const ranks = document.querySelectorAll('ul.ranked-list li')
+ 
+for (let i = 0; i < ranks.length; i++) {
+  ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+}
+
+}
+
+function deepestChild()
+{
+   var deep = document.getElementById('grand-node').querySelectorAll('div')
+   return deep[deep.length-1]
+  
+}
